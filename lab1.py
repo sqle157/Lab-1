5d2dd21d9b69ffc312c3e19e30c7dc71ec165d94
SANG LE <sqle@ualberta.ca>
2021-09-06 18:25:57 -0600

    fix Q10 (2)

--- lab1.py
+++ lab1.py
@@ -2,5 +2,5 @@ import requests
 
 print(requests.__version__)
 res = requests.get("http://google.com")
-r = requests.get("https://raw.githubusercontent.com/sqle157/Lab-1/main/lab1.py")
+r = requests.get("https://raw.githubusercontent.com/sqle157/Lab-1/main/lab1.py?flush_cache=True")
 print(r.text)
