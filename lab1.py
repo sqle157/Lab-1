8bba7aa7641ad435a61d08cf5db467f3f9538cef
SANG LE <sqle@ualberta.ca>
2021-09-06 18:20:03 -0600

    lab1-Q10

--- lab1.py
+++ lab1.py
@@ -2,3 +2,8 @@ import requests
 
 print(requests.__version__)
 res = requests.get("http://google.com")
+r = requests.get("https://raw.githubusercontent.com/sqle157/Lab-1/main/lab1.py")
+open('github', 'wb').write(r.content)
+fileName = open('github','r')
+for line in fileName :
+    print(line)
